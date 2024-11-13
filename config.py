2ddb0a46637b20ac3ec2c52db0e9860ff4b2a1bd
˹sᴡᴧɴ ꭙ 𝙲𝚎𝚖𝚊𝚛𝚊⁹⁹˼ 🇮🇩 🇮🇳 <158449266+XFLSWAN@users.noreply.github.com>
2024-11-13 15:12:27 +0530

    Update

---
+++ config.py
@@ -0,0 +1,6 @@
+# config.py
+
+API_ID = 'YOUR_API_ID'          # Telegram API ID from https://my.telegram.org
+API_HASH = 'YOUR_API_HASH'      # Telegram API Hash from https://my.telegram.org
+BOT_TOKEN = 'YOUR_BOT_TOKEN'    # Telegram Bot Token from @BotFather
+SESSION_NAME = 'YOUR_SESSION_NAME'  # Pyrogram session name for userbot (could be any unique name)
