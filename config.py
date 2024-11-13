a7942e876a5598d494874df4db91869e992edd26
˹sᴡᴧɴ ꭙ 𝙲𝚎𝚖𝚊𝚛𝚊⁹⁹˼ 🇮🇩 🇮🇳 <158449266+XFLSWAN@users.noreply.github.com>
2024-11-13 15:28:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # config.py
 
-API_ID = 'YOUR_API_ID'          # Telegram API ID from https://my.telegram.org
-API_HASH = 'YOUR_API_HASH'      # Telegram API Hash from https://my.telegram.org
-BOT_TOKEN = 'YOUR_BOT_TOKEN'    # Telegram Bot Token from @BotFather
-SESSION_NAME = 'YOUR_SESSION_NAME'  # Pyrogram session name for userbot (could be any unique name)
+API_ID = "27838385"         # Telegram API ID from https://my.telegram.org
+API_HASH = "0710bd2a89a41c3506f98f7e6fd7294a"      # Telegram API Hash from https://my.telegram.org
+BOT_TOKEN = "7317057245:AAFLFRb0C7BU9N_jFySHqSdp680ftcwg-g4"    # Telegram Bot Token from @BotFather
+SESSION_NAME = "BQGox7EAVcy12YuOUCsxDvGKYKbEn55girm-JzkmoLmi_iYzrlH0yiA6aSgBIVqzucdZyz2CWz-PCoi9fe-UpLU4JPB39jPxbccq_k26WYSqPVrczfrIg4I0iE5AugAetfniwZ24v36-m8u5M9jT0pPx_mdagQZRgdQiFMGppjePsoOs6h5STMgEffTcAAIaW3N-AqWap3xKPqsDVHo2dkK4dYz-NgS4CtzPyNi56nSPk7IMZ12T0c6PLZMCkjc1UAHanP9zRkzcmLCs6IfdbXIn6qUScK4dnbb1-WNGt-IDaHsd4tqNKp_0wmWBlFuJYJg3m3wKkiwh_8PwDjQvTa4sq7wAggAAAAGNehwsAA"  # Pyrogram session name for userbot (could be any unique name)
